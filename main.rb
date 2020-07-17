9d8f2f4a2048cfaafa4613331da4ee3888fe8e1a
abidoyinsola1 <ifeobayemi@gmail.com>
2020-07-17 13:38:00 +0100

    add my all enumerable

--- main.rb
+++ main.rb
@@ -18,16 +18,37 @@ end
 
 def my_select
   result = []
-  each do |i|
+  my_each do |i|
     result.push(i) if yield i
     end
    result
 end
 
+def my_all?
+    result = true
+    my_each do |i|
+    unless yield i 
+        result = false
+    end
+end
+    result
+end
+            
 end
 
   a = [1, 2, 3, 5] 
   a.my_each { |i| print  i * 4 }
   a.each_with_index { |i| puts "#{i} is the value" }
   puts a.my_select(&:even?)
+  puts(a.my_all? { |i| i > 0})
+
   
+# returns true or false. if both conditions are true, it returns true, else false
+
+# if true && true = true
+#     false && true = false
+#     true && false = false
+#     false && false = false
+
+#     false || true = true
+    
