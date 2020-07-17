c7a2d30b8db19f0980f69d49ddede66cc96d112d
abidoyinsola1 <ifeobayemi@gmail.com>
2020-07-17 14:54:45 +0100

    add my count

--- main.rb
+++ main.rb
@@ -6,8 +6,6 @@ module Enumerable
       self
     end
 
-
-
 def my_each_with_index
   0.upto(self.length-1) do |i|
     yield(self[i], i)
@@ -51,16 +49,23 @@ def my_none?
 end
   result
 end
+
+def my_count
+  return self.length unless block_given?
+count = 0
+my_each do |i|
+if yield i
+  count += 1 
+end
+end
+count
+end
             
 end
 
-  a = [1, 2, 3, 5] 
-  a.my_each { |i| print  i * 4 }
-  a.each_with_index { |i| puts "#{i} is the value" }
-  puts a.my_select(&:even?)
-  puts(a.my_all? { |i| i > 1})
-  puts(a.my_any? { |i| i > 1})
-  puts(a.my_none?{ |i| i > 1})
-  puts hope that we didint 
-
-    
+a = [1, 2, 3, 5] 
+a.my_each { |i| print  i * 4 }
+a.each_with_index { |i| puts "#{i} is the value" }
+puts a.my_select(&:even?)
+puts(a.my_all? { |i| i > 0})
+puts(a.my_count)
