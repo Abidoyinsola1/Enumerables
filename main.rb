cf8d9b5340483fa771ddbbe5fce417b36ac77e97
rahalrazika <rere055@gmail.com>
2020-07-17 12:19:25 +0200

    add my_select method

--- main.rb
+++ main.rb
@@ -6,18 +6,28 @@ module Enumerable
       self
     end
 
-end
+
 
 def my_each_with_index
-    0.upto(self.length-1) do |i|
-      yield(self[i], i)
-    end
-
-    self
+  0.upto(self.length-1) do |i|
+    yield(self[i], i)
   end
 
+  self
+end
+
+def my_select
+  result = []
+  each do |i|
+    result.push(i) if yield i
+    end
+   result
+end
+
+end
 
   a = [1, 2, 3, 5] 
   a.my_each { |i| print  i * 4 }
-
-  a.each_with_index { |i| puts i[1] }
+  a.each_with_index { |i| puts "#{i} is the value" }
+  puts a.my_select(&:even?)
+  
