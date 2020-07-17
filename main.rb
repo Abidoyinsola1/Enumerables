9a115ed2fdeb85e44f2e35f8be794c23b8182032
abidoyinsola1 <ifeobayemi@gmail.com>
2020-07-17 09:15:56 +0100

    add my each with index

--- main.rb
+++ main.rb
@@ -5,12 +5,19 @@ module Enumerable
       end
       self
     end
+
+end
+
+def my_each_with_index
+    0.upto(self.length-1) do |i|
+      yield(self[i], i)
+    end
+
+    self
   end
-
- 
-
 
 
   a = [1, 2, 3, 5] 
-     
-  a.my_each { |i| print  i  }
+  a.my_each { |i| print  i * 4 }
+
+  a.each_with_index { |i| puts i[1] }
