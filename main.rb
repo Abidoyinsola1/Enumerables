f089a0078ac52a13eb66ae989efc87926191a4b0
rahalrazika <rere055@gmail.com>
2020-07-23 08:58:47 +0200

    update the  methods

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    return unless block_given?
+    return to_enum(:my_each) unless block_given?
 
     each do |i|
       yield i
@@ -9,16 +9,15 @@ module Enumerable
   end
 
   def my_each_with_index
-    return unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     0.upto(length - 1) do |i|
-      yield i, index
-      index + 1
+      yield self[i], i
     end
   end
 
   def my_select
-    return unless block_given?
+    return to_enum(:my_select) unless block_given?
 
     result = []
     my_each do |i|
@@ -40,35 +39,38 @@ module Enumerable
   end
 
   def my_any?(*val)
-    result = true
+    result = false
     if !val[0].nil?
-      my_each { |i| result = false unless val[0] == i }
+      my_each { |i| result = true if val[0] == i }
     elsif !block_given?
-      my_each { |i| result = false unless i }
+      my_each { |i| result = true if i }
     else
-      my_each { |i| result = false unless yield(i) }
+      my_each { |i| result = true if yield(i) }
     end
     result
   end
 
   def my_none?(*val)
-    result = false
+    result = true
     if !val[0].nil?
-      my_each { |i| result = false unless val[0] == i }
+      my_each { |i| result = false if val[0] == i }
     elsif !block_given?
-      my_each { |i| result = false unless i }
+      my_each { |i| result = false if i }
     else
       my_each { |i| result = false if yield(i) }
     end
     result
   end
 
-  def my_count(*)
-    return length unless block_given?
-
+  def my_count(val = nil)
     count = 0
-    my_each do |i|
-      count += 1 if yield i
+
+    if !val.nil?
+      my_each { |i| count += 1 if i == val }
+    elsif block_given?
+      my_each { |i| count += 1 if yield(i) }
+    else
+      my_each { count += 1 }
     end
     count
   end
@@ -88,35 +90,30 @@ module Enumerable
     result
   end
 
-  def my_inject(first_aug = nil)
-    i = 0
-    acc = first_aug
-    sym = (first_aug if first_aug == Symbol || String)
+  def my_inject(first_aug = nil, sym = nil)
     if block_given?
-      while i < length
-        if acc.nil?
-          acc = self[0]
-          i += 1
-          next
-        end
-        acc = yield(acc, self[i])
-        i += 1
-      end
-    elsif !sym.nil? && sym.class == Symbol
-      while i < length
-        acc = acc.send(sym, self[i])
-        i += 1
-      end
-    elsif !sym.nil? && sym.class == String && %r{[+-/*]}.match(sym)
-      while i < length
-        acc = acc.send(sym, self[i])
-        i += 1
-      end
+      acc = first_aug
+      my_each { |i| acc = acc.nil? ? i : yield(acc, i) }
+      raise LocalJumpError unless block_given? || !sym.empty? || !first_aug.empty?
+
+      acc
+    elsif !first_aug.nil? && (first_aug if first_aug == Symbol || String)
+      raise LocalJumpError unless block_given? || !first_aug.empty?
+
+      acc = nil
+      my_each { |i| acc = acc.nil? ? i : acc.send(first_aug, i) }
+      acc
+    elsif !sym.nil? && (sym if sym == Symbol || String)
+      raise LocalJumpError unless block_given? || !sym.empty?
+
+      acc = first_aug
+      my_each { |i| acc = acc.nil? ? i : acc.send(sym, i) }
+      acc
+    else
+      raise LocalJumpError
     end
-    acc
   end
 end
-
 def multiply_els(arr)
   arr.my_inject(1) { |val, i| val * i }
 end
